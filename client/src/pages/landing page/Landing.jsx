9b138042a900b823d9d678951dc94da7c695bae1
unknown <swastikdubey1234@gmail.com>
2022-09-11 01:50:54 +0530

    head line

--- client/src/pages/landing page/Landing.jsx
+++ client/src/pages/landing page/Landing.jsx
@@ -81,6 +81,22 @@ const Landing = () => {
         </div>
       </section>
 
+
+<section>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12 text-center">
+      <h3 class="animate-charcter">Get prepared for the JEE classes with mock test facilities. Waiting is over! Join our Academic session today to make your future bright ( For for ARTS/SCIENCE/COMMERCE students)</h3>
+    </div>
+  </div>
+</div>
+</section>
+
+
+
+
+
+
       <section>
         <div class="wrapper">
           <ol>
